706482f9f1e36d5cb425e2b3adee391ee53bd9ef
RuhanKarimov <ruhankarimovwork@gmail.com>
2025-03-22 15:54:55 +0300

    Pom.xml güncellenmiştir maven ı sync edin.

---
+++ src/main/java/com/example/demo/Service/ImdbService.java
@@ -0,0 +1,41 @@
+package com.example.demo.Service;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImdbService {
+    public String imdbIDGet(String filmAdi){
+        filmAdi=filmAdi.replace(" ","+");
+        String url = "https://www.imdb.com/find?q=";
+        String imdbID = null;
+        try{
+            Document doc = Jsoup.connect(url).get();
+            Element film = doc.select("a[href^=/title/tt]").first();
+            if (film != null) {
+                String href = film.attr("href");
+                imdbID = href.split("/")[2];
+                return imdbID;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String imdbRatingGet(String filmAdi){
+        String imdbID = imdbIDGet(filmAdi);
+        String url = "https://www.imdb.com/"+imdbID;
+        try{
+            Document doc = Jsoup.connect(url).get();
+            String rating = doc.select(".sc-d541859f-1").text();
+            return rating;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
